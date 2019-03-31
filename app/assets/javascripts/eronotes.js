fed48fcf242c63fd1ffa5d40ceef478cdbf51e69
joselabaki <jose.labaki@hotmail.com>
2019-03-31 15:54:26 +0200

    slicing

--- app/assets/javascripts/eronotes.js
+++ app/assets/javascripts/eronotes.js
@@ -17,7 +17,7 @@ customBtn.addEventListener("click", function() {
     if (realFileBtn.value) {
       customTxt.innerHTML =  realFileBtn.value.match(
         /[\/\\]([\w\d\s\.\-\(\)]+)$/
-      )[1];
+      )[1].slice(0,14) + " ...";
 
       customUpBtn.style.color = "#E2DEDC";
       customUpBtn.classList.remove("disabled")
