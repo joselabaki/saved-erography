7f92f6b17d396c77a07b9b3ddc9ea8cb303363ea
joselabaki <jose.labaki@hotmail.com>
2019-04-06 12:50:15 +0200

    erro-page'

---
+++ app/controllers/errors_controller.rb
@@ -0,0 +1,39 @@
+class ErrorsController < ApplicationController
+
+
+
+def not_found
+  render status: 404
+end
+
+def forbidden
+  render status: 422
+
+end
+
+def server_error
+  render status: 500
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'eronotes#new'
+    get '/404' , to: 'errors#not_found'
+    get '/422' , to: 'errors#forbidden'
+    get '/500' , to: 'errors#server_error'
+
 
   end
 end
